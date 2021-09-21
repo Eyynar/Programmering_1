16d6bbc96ac33444e2ed6d0b5b83dba9619faf71
Eyynar <89594643+Eyynar@users.noreply.github.com>
2021-09-21 12:34:11 +0200

    La til resten av oppgave 1

--- Forelesning 8/tuples.py
+++ Forelesning 8/tuples.py
@@ -1,3 +1,13 @@
 top_board_games = ("Marvel United", "Lost Ruins of Arnak", "Dune: Imperium")
 
 print(top_board_games)
+
+for board_game in top_board_games:
+    print(board_game)
+
+# top_board_games.append("Risk")
+# top_board_games.pop()
+# top_board_games[0] = "Risk"
+# top_board_games.sort()
+
+print(sorted(top_board_games))
