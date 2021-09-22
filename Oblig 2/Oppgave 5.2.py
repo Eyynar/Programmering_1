dd501de5706acafabca41bbfd42c4fd7106c7fca
Eyynar <89594643+Eyynar@users.noreply.github.com>
2021-09-22 12:19:40 +0200

    Forbedring av oppgave 5

---
+++ Oblig 2/Oppgave 5.2.py
@@ -0,0 +1,25 @@
+import random
+
+points = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+          [2, 4, 6, 7, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+          [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60],
+          [25, 50]]
+
+number_of_players = int(input("Input the number of players: "))
+number_of_rounds = int(input("Input the number of round the players will play: "))
+number_of_throws = int(input("Input number of throws each player throws per round? "))
+
+for h in range(number_of_rounds):
+    print(f"\n\n=============== Round {h + 1} ===============")
+
+    for i in range(number_of_players):
+        print(f"\n\n===== Player {i + 1} ===== ")
+
+        total = 0
+        for j in range(number_of_throws):
+            random_list = random.randint(0, len(points)-1)
+            score = points[random_list][random.randint(0, len(points[random_list])-1)]
+            print(f"Throw {j + 1}: {score}")
+            total += score
+
+        print(f"\nTotal for player {i + 1}: {total}")
