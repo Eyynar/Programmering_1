f4fa5911b38484df0067b6e1f2b24849bc79041e
Eyynar <89594643+Eyynar@users.noreply.github.com>
2021-11-08 17:43:04 +0100

    La til bilde

--- Oblig 5/project.py
+++ Oblig 5/project.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import json
-
+from PIL import ImageTk, Image
 
 class Game:
     def __init__(self, title, release, score, developer, image):
@@ -13,6 +13,7 @@ class Game:
 
 def add_game(title, release, score, developer, image):
     games.append(Game(title, release, score, developer, image).__dict__)
+    save_to_file()
 
 
 def edit_game(index, new_title, new_release, new_score, new_developer, new_image):
@@ -21,49 +22,45 @@ def edit_game(index, new_title, new_release, new_score, new_developer, new_image
     games[index]["score"] = new_score
     games[index]["developer"] = new_developer
     games[index]["image"] = new_image
+    save_to_file()
 
 
-def item_selected(*args):
-    game_key = listbox_movies.get(listbox_movies.curselection())
-    game = games[game_key]
-
-    ent_game_title.delete(0, tk.END)
-    ent_game_title.insert(0, game['title'])
-    ent_movie_year.delete(0, tk.END)
-    ent_movie_year.insert(0, game['year'])
-    ent_movie_duration.delete(0, tk.END)
-    ent_movie_duration.insert(0, game['duration'])
-
-
-def save_game():
+def display_game(event):
     pass
+
+
+def save_to_file():
+    with open("test.json", "w") as output_file:
+        json.dump(games, output_file, indent=4)
 
 
 games = []
 
 add_game("The Outer Wilds", 2019, 10, "blank", 2)
+add_game("Hollow Knight", 2017, 10, "Team Cherry", 2)
 
+window = tk.Tk()
 
-with open("test.json", "w") as output_file:
-    json.dump(games, output_file, indent=4)
+# Listbox og frame
+listbox_frame = tk.Frame()
+listbox = tk.Listbox(listbox_frame)
 
+listbox.pack()
+listbox_frame.pack(side=tk.LEFT)
 
+listbox.bind("<<ListboxSelect>>", display_game)
+
+# Legger til elementer i lista
 with open("test.json", "r") as input_file:
-    print(json.load(input_file))
+    games_list = json.load(input_file)
+    for i in range(len(games_list)):
+        listbox.insert(i, games_list[i]["title"])
 
-window = tk.Tk();
 
-# List frame
-list_frame = tk.Frame()
-
-# TODO Fikse tittel på i listen
-game_list = tk.StringVar(value=list(games))
-listbox_movies = tk.Listbox(list_frame, listvariable=game_list)
-listbox_movies.bind('<<ListboxSelect>>', item_selected)
-
-listbox_movies.pack()
-list_frame.pack(side=tk.LEFT)
-
+canvas = tk.Canvas(window, width = 300, height = 300)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open("images/theouterwilds.jpg"))
+canvas.create_image(20, 20, anchor="nw", image=img)
 
 # Main frame
 main_frame = tk.Frame()
@@ -74,21 +71,22 @@ lbl_movie_year = tk.Label(main_frame, text="Year:")
 lbl_movie_duration = tk.Label(main_frame, text="Duration:")
 
 # Form entries
-ent_game_title = tk.Entry(main_frame)
+ent_movie_title = tk.Entry(main_frame)
 ent_movie_year = tk.Entry(main_frame)
 ent_movie_duration = tk.Entry(main_frame)
 
-btn_save = tk.Button(main_frame, text="Save", command=save_game)
+# btn_save = tk.Button(main_frame, text="Save", command=save_movie)
 
 
 lbl_movie_title.grid(row=0, column=0)
 lbl_movie_year.grid(row=1, column=0)
 lbl_movie_duration.grid(row=2, column=0)
-ent_game_title.grid(row=0, column=1)
+ent_movie_title.grid(row=0, column=1)
 ent_movie_year.grid(row=1, column=1)
 ent_movie_duration.grid(row=2, column=1)
-btn_save.grid(row=3, column=0, columnspan=2)
+# btn_save.grid(row=3, column=0, columnspan=2)
 
 main_frame.pack()
+
 
 window.mainloop()
